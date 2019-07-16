274b33b4abd5563ea5c2896c226524a6621c5548
Jonathan Taylor <jonathan.taylor@stanford.edu>
2019-07-16 13:29:19 -0700

    trying python dataframe encoding

--- preprocessors.py
+++ preprocessors.py
@@ -111,9 +111,17 @@ class SelectiveInferencePreprocessor(ExecutePreprocessor):
             capture_cell = nbformat.v4.new_code_cell()
             for selection in cell.metadata['capture_selection']:
                 if self.km.kernel_name == 'python3':
-                    capture_cell.source += '\n'.join(['from IPython.display import display',
-                                                      'import json',
-                                                      'display({"application/selective.inference":json.dumps(%s)}, metadata={"encoder":"json"}, raw=True)' % selection['name']])
+                    if selection['encoder'] == 'json':
+                        capture_cell.source += '\n'.join(['from IPython.display import display',
+                                                          'import json',
+                                                          'display({"application/selective.inference":json.dumps(%s)}, metadata={"encoder":"json"}, raw=True)' % selection['name']])
+                    elif selection['encoder'] == 'dataframe':
+                        capture_cell.source += '\n'.join(['from IPython.display import display',
+                                                          'import json, tempfile',
+                                                          'filename = tempfile.mkstemp()[1]',
+                                                          'feather.write_dataframe(%s, filename)',
+                                                          'display({"application/selective.inference":open(filename, "rb").read(), metadata={"encoder":"dataframe"}, raw=True)' % selection['name']])
+
                 elif self.km.kernel_name == 'ir':
                     capture_cell.source += '''
 IRdisplay:::display_raw("application/selective.inference", FALSE, toJSON(%s), NULL, list(encoder="json"))
@@ -128,7 +136,6 @@ IRdisplay:::display_raw("application/selective.inference", FALSE, toJSON(%s), NU
                 decoder = {'json':json.loads,
                            'dataframe':base64_to_dataframe,
                            }.get(output.metadata['encoder'], 'json')
-                stop
                 {'set':set_selection,
                  'fixed':fixed_selection}[selection['selection_type']].setdefault(selection['name'], decoder(output_data))
 
