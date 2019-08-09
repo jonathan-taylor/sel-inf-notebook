072a18a23c7d1c9fb1b481af8fb4d73b8f3a95c8
Alex Kim <alexkim7@stanford.edu>
2019-08-08 17:17:58 -0700

    Add documentation to json_dataframe.py

--- json_dataframe.py
+++ json_dataframe.py
@@ -1,10 +1,14 @@
-import tempfile, os, json
-import numpy as np
+import base64
 import feather
+import json
+import numpy as np
+import os
+import tempfile
 from IPython.display import display
 
+# R code to write a dataframe to a feather and display the raw bytecode
 dataframe_to_jsonR = '''
-dataframe_to_display = function(D) {
+dataframe_to_display <- function(D) {
     filename = tempfile()
     feather::write_feather(D, filename)
     A = readBin(file(filename, 'rb'), 'raw', file.size(filename) + 1000)
@@ -13,11 +17,32 @@ dataframe_to_display = function(D) {
 '''
 
 def base64_to_dataframe(base64_data):
-    byte_str = base64.b64decode(base64_data)
+    """Decodes a string of base 64 data into a dataframe. Assumes the
+    input string is the raw base 64 encoding of a feather file.
+
+    Parameters
+    ----------
+    base64_data : string
+        A string representing the base 64 encoding of a feather file.
+
+    Returns
+    -------
+    dataframe : Pandas dataframe
+        The dataframe reconstructed from the given base 64 code.
+    """
     tmpfile = tempfile.mkstemp()[1]
+    byte_str = base64.b64decode(base64_data)  # raw bytecode from base64
+
+    # Write the raw bytecode into a tempfile (i.e. reconstruct the
+    # feather file)
     with open(tmpfile, 'wb') as f:
         f.write(byte_str)
-    return feather.read_dataframe(tmpfile)
+
+    # Read the feather file into a dataframe
+    dataframe = feather.read_dataframe(tmpfile)
+
+    return dataframe
+
 
 def dataframe_to_display(data_frame):
     """Save an array of floats to JSON.
@@ -37,6 +62,3 @@ def dataframe_to_display(data_frame):
     
     # Return the JSON representation of the list of ints
     return json.dumps(array_bytes_as_ints)
-
-
-
